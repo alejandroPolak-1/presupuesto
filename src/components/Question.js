956d57b6570dc4f6942732200191effc6099a3ca
alejandroPolak <alejandro_polak@hotmail.com>
2020-04-30 12:49:54 -0300

    [useState]
    e.preventDefault() para evitar que se envie el query string por metodo get
     y se recarge la pagina

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,34 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Question = () => {
+  //Definir State
+  const [count, setCount] = useState(0)
+
+  //Función que lee el presupuesto
+  const handleChange = (e) => {
+    setCount(parseInt(e.target.value, 10))
+  }
+
+  //Submit para definir presumuesto
+  const handleSubmit = (e) => {
+      e.preventDefault();
+
+      //Validar
+
+      //Si se pasa la validación
+  }
+        
+
   return (
     <Fragment>
       <h2> Coloca tu presupuesto</h2>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={handleChange}
         />
         <input
           type="submit"
